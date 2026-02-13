e0875b7395e635c2e514bf87ae6c8439e3316101
AnctSinps01 <jiyouan_0325@163.com>
2026-02-13 12:04:15 +0800

    add: todo list / map

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,13 +8,16 @@ public class MapGenerator : MonoBehaviour
     
     [Header("地块预制体")]
     public GameObject tilePrefab;
-    
-    [Header("墙壁设置")]
-    public float wallThickness = 0.5f;
+
     
     void Start()
     {
         GenerateMap();
+    }
+
+    void Update()
+    {
+        //GenerateMap();
     }
     
     void GenerateMap()
@@ -36,48 +39,5 @@ public class MapGenerator : MonoBehaviour
                 tile.name = $"Tile_{x}_{y}";
             }
         }
-        
-        // 生成边界空气墙
-        GenerateWalls(startX, endX, startY, endY);
-    }
-    
-    void GenerateWalls(int startX, int endX, int startY, int endY)
-    {
-        // 计算地图边界中心点和大小
-        float centerX = 0;
-        float centerY = 0;
-        float mapWidth = width + 1;  // 包含边界
-        float mapHeight = height + 1;
-        
-        // 上墙
-        CreateWall("Wall_Top", 
-            new Vector3(centerX, endY + 0.5f + wallThickness / 2, 0),
-            new Vector2(mapWidth, wallThickness));
-        
-        // 下墙
-        CreateWall("Wall_Bottom", 
-            new Vector3(centerX, startY - 0.5f - wallThickness / 2, 0),
-            new Vector2(mapWidth, wallThickness));
-        
-        // 左墙
-        CreateWall("Wall_Left", 
-            new Vector3(startX - 0.5f - wallThickness / 2, centerY, 0),
-            new Vector2(wallThickness, mapHeight));
-        
-        // 右墙
-        CreateWall("Wall_Right", 
-            new Vector3(endX + 0.5f + wallThickness / 2, centerY, 0),
-            new Vector2(wallThickness, mapHeight));
-    }
-    
-    void CreateWall(string name, Vector3 position, Vector2 size)
-    {
-        GameObject wall = new GameObject(name);
-        wall.transform.position = position;
-        wall.transform.SetParent(transform);
-        wall.tag = "Wall";
-        
-        BoxCollider2D collider = wall.AddComponent<BoxCollider2D>();
-        collider.size = size;
     }
 }
